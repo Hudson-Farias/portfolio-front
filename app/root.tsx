087e8775290a1e6ab9199de00415b8fd212295c3
Hudson Farias <hudson.farias.dev@gmail.com>
2025-02-28 13:50:00 -0300

    analytics

--- app/root.tsx
+++ app/root.tsx
@@ -33,6 +33,23 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-473439473`}
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-473439473');
+            `,
+          }}
+        />
+
       </head>
       <body className="font-dmSans py-4">
         <Nav />
